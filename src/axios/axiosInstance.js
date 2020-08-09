d9fc32c18d0848cd9d91c78e51cdd34fa4be33e8
Namrata <namrata@pratian.com>
2020-08-09 14:21:47 +0530

    axios config

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const instance = axios.create({
+//     baseURL : 'http://localhost:5003'
+// })
+
 const instance = axios.create({
-    baseURL : 'http://localhost:5003'
+    baseURL : 'https://userdeptassignment-be.herokuapp.com/'
 })
 
 export default instance
